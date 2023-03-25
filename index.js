1cfc079bd670d1b40ab574ca3c8dbacd70ccc962
lordish <gitonga403@gmail.com>
2023-03-25 10:09:24 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
-
+  fetch ('https://anapioficeandfire.com/api/books')
+  .then(function (response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(renderBooks(data));
+  })
+} 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
